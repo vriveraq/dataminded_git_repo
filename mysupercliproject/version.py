05d6a0402c845d68ad3d197928013bc6f4cd836a
Cedric De Boom <cedric.deboom@vlaanderen.be>
2023-02-08 10:30:42 +0000

    Change release version

--- mysupercliproject/version.py
+++ mysupercliproject/version.py
@@ -9,4 +9,4 @@ This module contains project version information.
 """
 
 __version__ = "9.9.9"  #: the working version
-__release__ = "0.0.1"  #: the release version
+__release__ = "0.0.2"  #: the release version
